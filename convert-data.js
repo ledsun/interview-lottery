51d4ae9521f88a770b3273515c70fcb406e956bb
ledsun <shigeru.nakajima@gmail.com>
2018-01-11 11:06:36 +0900

    Change data format of doneList in the data.json to enable to include multiple interviewees

--- convert-data.js
+++
@@ -1,7 +0,0 @@
-const oldData = require('./data.example')
-const converted = oldData.doneList.map((done) => done.reduce((map, d, index) => {
-  map[data2.interviewers[index]] = d
-  return map
-}, {}))
-
-console.log(JSON.stringify(converted,null,2));
